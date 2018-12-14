b09146c7b6a9cbbacb0518ccdf378f7d76f9e8a5
Hornibrook, Connor Francis <hornibrookc@rowan.edu>
2018-12-14 09:01:12 -0500

    fixed name mangling issue for apis

--- apis/utils/Api.py
+++ apis/utils/Api.py
@@ -1,5 +1,4 @@
 import abc
-from urllib.parse import quote
 
 
 class Api(abc.ABC):
@@ -8,7 +7,7 @@ class Api(abc.ABC):
 
         self.public_key       = in_public_key
         self.private_key      = in_private_key
-        self.url = in_url
+        self.url              = in_url
         self.ip_restriction   = in_ip_restriction
         self.endpoints        = []
         self.endpoint_to_args = {}
@@ -22,9 +21,10 @@ class Api(abc.ABC):
         if endpoint_name in self.endpoints and \
                 all(arg in self.endpoint_to_args[endpoint_name] for arg in kwargs.keys()):
 
-            self.__update_time()
+            self._update_time()
             params = dict(self.authentication_parameters, **kwargs)
-            return self.url + parse_parameters(params)
+            return self.url + "{0}?{1}".format(endpoint_name, "&".join("{0}={1}".format(k, v)
+                                                                       for k, v in params.items()))
         else:
             return None
 
@@ -39,30 +39,21 @@ class Api(abc.ABC):
         return {}
 
     @abc.abstractmethod
-    def __set_args_mapping(self):
+    def _set_args_mapping(self):
         pass
 
     @abc.abstractmethod
-    def __set_endpoints(self):
+    def _set_endpoints(self):
         pass
 
     @abc.abstractmethod
-    def __hit_endpoint(self, valid_args, endpoint_name, get_one=False, request_type="GET", **kwargs):
+    def _hit_endpoint(self, valid_args, endpoint_name, get_one=False, request_type="GET", **kwargs):
         pass
 
     @abc.abstractmethod
-    def __generate_hash_value(self):
+    def _generate_hash_value(self):
         pass
 
     @abc.abstractmethod
-    def __update_time(self):
+    def _update_time(self):
         pass
-
-
-def parse_parameters(parameters):
-    """
-    Parses a dictionary of URL parameters into a string
-    :param parameters: A dict of parameters (dict)
-    :return:           A parsed string of url params e.g. "&name=john&state=nj"
-    """
-    return quote("&".join(["{0}={1}".format(parameter, value) for parameter, value in parameters.items()]))
