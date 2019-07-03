b6046edb6dc48ff273dee437930308a8b7bf65d4
hornibrookc <hornibrookc@rowan.edu>
2019-07-03 11:07:24 -0400

    update to with_oracle_connection

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ def read(file_name):
 
 
 setup(
-    name='profpy',
-    version='0.4.2',
-    packages=['profpy', 'profpy.db', 'profpy.db.fauxrm', 'profpy.db.fauxrm.queries', 'profpy.db.fauxrm.handlers', 'profpy.db.general', 'profpy.apis', 'profpy.apis.utils'],
-    url='https://github.com/rowanuniversity/profpy/',
+    name="profpy",
+    version="0.4.2",
+    packages=["profpy", "profpy.db", "profpy.db.fauxrm", "profpy.db.fauxrm.queries", "profpy.db.fauxrm.handlers", 
+              "profpy.db.general", "profpy.apis", "profpy.apis.utils"],
+    url="https://github.com/rowanuniversity/profpy/",
     license="MIT",
-    author='Connor Hornibrook',
-    author_email='hornibrookc@rowan.edu',
+    author="Connor Hornibrook",
+    author_email="hornibrookc@rowan.edu",
     install_requires=["cx_Oracle", "requests"],
-    description='',
-    long_description=read("README.md")
+    description="",
+    long_description=read("README.md"),
+    long_description_type="text/markdown"
 )
