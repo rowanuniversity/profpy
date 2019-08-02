5db43072b8ee59a3dddaba7feacac0f1cce0ae86
hornibrookc <hornibrookc@rowan.edu>
2019-08-02 10:28:58 -0400

    rm old unit tests

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Creating tables, if needed..."
-python3 test/create_test_tables.py
-echo "Done."
-echo "Running tests..."
-python3 -m unittest discover -s test
-echo "Removing test tables..."
-python3 test/remove_test_tables.py
-echo "Done."
